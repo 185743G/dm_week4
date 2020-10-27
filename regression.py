6ac0943d2b0d982d19642687647864c6b2432068
Yusuke OSIRO <e185743@ie.u-ryukyu.ac.jp>
2020-10-27 17:14:16 +0900

    add regression_fit

--- regression.py
+++ regression.py
@@ -5,7 +5,17 @@ class LinearRegression:
 	y = None
 	
 	def fit(self,x,y):
-		pass
+		temp = np.linalg.inv(np.dot(x.T,x))
+		self.theta = np. dot(np.dot(temp,x.T),y)
+		"""testing(cont.)
+		import importlib
+		importlib,reload(regression)
+		model = regression.LinearRegression()
+		model.fit(X,Y)
+		model.theta
+		expect
+		array([5.30412371,0.49484536])
+		"""
 	def predict(self,x):
 		pass
 	def score(self,x,y):
