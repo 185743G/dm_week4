25494037e92b2b1d5f8f176605f8246a6d3ab115
Yusuke OSIRO <e185743@ie.u-ryukyu.ac.jp>
2020-10-27 17:19:28 +0900

    add regression_predict

--- regression.py
+++ regression.py
@@ -17,7 +17,16 @@ class LinearRegression:
 		array([5.30412371,0.49484536])
 		"""
 	def predict(self,x):
-		pass
+		return np.dot(x,self.theta)
+		"""testing(cont.)
+		importlib,reload(regression)
+		model = regression.LinearRegression()
+		model.fit(X,Y)
+		model.predict(X)
+		expect
+		array([7.2,9.2,11.7,13.7])
+		"""
 	def score(self,x,y):
+		
 		pass
 	
