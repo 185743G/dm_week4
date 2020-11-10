5d8c5057a4af002ffda8dafb17af27227ce4eb57
Yusuke OSIRO <e185743@ie.u-ryukyu.ac.jp>
2020-11-10 17:26:03 +0900

    expand poly2_and_poly3

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import datasets
-X,Y = datasets.load_linear_example1()
-print(X)
+# X,Y = datasets.load_linear_example1()
+
+X,Y = datasets.load_nonlinear_example1()
+
+# ex_X = datasets.polynomial2_features(X)
+ex_X = datasets.polynomial3_features(X)
+
+print(ex_X)
 print(Y)
 
 import regression
@@ -11,6 +17,6 @@ import importlib
 importlib.reload(regression)
 model = regression.LinearRegression()
 model.fit(X,Y)
-print(model.theta)
-print(model.predict(X))
-print(model.score(X,Y))
+# print(model.theta)
+# print(model.predict(X))
+# print(model.score(X,Y))
